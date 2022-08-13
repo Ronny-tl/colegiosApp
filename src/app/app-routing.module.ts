c4b5616ece1973cc60519ff90f89b4cd38c93b5a
DESKTOP-57IG3O7\MRX <ron29b@gmail.com>
2022-08-13 14:57:02 -0500

    creamos el cliente Angular v14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'loginAdmin', loadChildren: () => import ('./components/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'loginAlumno', loadChildren: () => import ('./components/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'loginDocente', loadChildren: () => import ('./components/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'loginTutor', loadChildren: () => import ('./components/login/login.module').then(m => m.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
