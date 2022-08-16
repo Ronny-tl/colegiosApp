80912da21f68f23fa11a10fd1a6fee87f2579b7c
DESKTOP-57IG3O7\MRX <ron29b@gmail.com>
2022-08-15 20:03:52 -0500

    CRUD COMPLETO LISTAR DOCENTES PANEL ADMIN

--- src/app/components/panel/admin/listar-docentes/listar-docentes.component.ts
+++ src/app/components/panel/admin/listar-docentes/listar-docentes.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+import { ToastService } from 'src/app/shared/services/toast.service';
+import { DocenteService } from '../../services/docente.service';
 
+declare var $: any;
 @Component({
   selector: 'app-listar-docentes',
   templateUrl: './listar-docentes.component.html',
@@ -7,9 +12,103 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarDocentesComponent implements OnInit {
 
-  constructor() { }
+  listData:any[] = [];
+  formDocente:FormGroup;
+
+  codigoProfesor = 'codigoProfesor';
+  nombres = 'nombres';
+  apellidos = 'apellidos';
+  tipoDocumento = 'tipoDocumento';
+  numDocumento = 'numDocumento';
+
+  operacion:string;
+  constructor(
+    private alumnoService: DocenteService,
+    private _fb:FormBuilder,
+    private toastService: ToastService
+  ) { 
+    this.crearFormulario();
+  }
 
   ngOnInit(): void {
+    this.getDocentes();
+
   }
+
+  crearFormulario(){
+    this.formDocente = this._fb.group({
+      [this.codigoProfesor]: [null],
+      [this.nombres]: [null, [Validators.required]],
+      [this.apellidos]: [null, [Validators.required]],
+      [this.tipoDocumento]: [null, [Validators.required]],
+      [this.numDocumento]: [null, [Validators.required]],
+    })
+  }
+
+  getDocentes(){
+    this.alumnoService.getDocentes().subscribe(response => {
+      this.listData = response;
+    })
+  }
+
+
+  openModalDocente(val: boolean){
+    $('#modalDocente').modal(val ? 'show' : 'hide'); 
+  }
+
+  openModalEditar(item){
+    this.operacion = 'Editar'
+    this.formDocente.patchValue({
+      [this.codigoProfesor]: item.codigoProfesor,
+      [this.nombres]: item.nombres ,
+      [this.apellidos]: item.apellidos ,
+      [this.tipoDocumento]: item.tipoDocumento,
+      [this.numDocumento]: item.numDocumento,
+    })
+    
+    this.openModalDocente(true);
+  }
+
+  editar(){
+    let data = {...this.formDocente.value}
+    console.log("ANTES DE ENVIAR", data);
+    data.codigoProfesor = Number(data.codigoProfesor);
+    data.fechaNacimiento = moment(new Date(data.fechaNacimiento)).format('YYYY-MM-DD')
+    this.alumnoService.updateDocente(data).subscribe(response => {
+      console.log("RESPONSE UPDATE", response);
+      this.toastService.toast('success', 'Exito', 'Docente actualizado exitosamente');
+      this.getDocentes();
+      this.openModalDocente(false);
+    },err => {
+      console.log(err);
+      this.toastService.toast('error', 'Error', 'Ocurrio un problema por favor pongase en contacto con el administrador');
+    })
+  }
+  agregar(){
+    this.formDocente.get(this.codigoProfesor).reset();
+    let data = {...this.formDocente.value}
+    data.codigoProfesor = Number(data.codigoProfesor);
+    data.fechaNacimiento = moment(new Date(data.fechaNacimiento)).format('YYYY-MM-DD')
+    this.alumnoService.setDocentes(data).subscribe(response => {
+      console.log("RESPONSE", response);
+      this.toastService.toast('success', 'Exito', 'Docente agregado exitosamente');
+      this.getDocentes();
+      this.openModalDocente(false);
+    },err => {
+      console.log(err);
+      this.toastService.toast('error', 'Error', 'Ocurrio un problema por favor pongase en contacto con el administrador');
+    })
+  }
+
+  eliminar(item){
+    this.alumnoService.deleteDocente(item.codigoProfesor).subscribe(response => {
+      this.toastService.toast('success', 'Exito', 'Docente eliminado exitosamente');
+      this.getDocentes();
+    },err => {
+      console.log(err);
+      this.toastService.toast('error', 'Error', 'Ocurrio un problema por favor pongase en contacto con el administrador');
+    })
+  }
+
 
 }
--- src/app/components/panel/services/docente.service.ts
+++ src/app/components/panel/services/docente.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocenteService {
+  urlBase = environment.urlBackend;
+  constructor(private _http: HttpClient) { }
 
-  constructor() { }
+  getDocentes(): Observable<any>{
+    return this._http.get<any[]>(`${this.urlBase}docentes/`)
+  }
+
+  getApoderados(): Observable<any>{
+    return this._http.get<any[]>(`${this.urlBase}tutor`)
+  }
+
+  setDocentes(dataJson): Observable<any>{
+    return this._http.post(`${this.urlBase}docentes/`, dataJson)
+  }
+
+  updateDocente(dataJson): Observable<any>{
+    return this._http.put(`${this.urlBase}docentes/${dataJson.codigoProfesor}/`, dataJson)
+  }
+
+  deleteDocente(codigoProfesor){
+    return this._http.delete(`${this.urlBase}docentes/${codigoProfesor}/`)
+  }
 }
